d28fd6f6823c6c95c79bd92b9c969b27f3f4962e
dan4th4 <danylo.sumtsov@gmail.com>
2023-01-12 01:00:24 +0200

    Add setImageAssets function for be able to load image resources from assets folder Add changeVisibility function for toggling views visibility value Add isValidEmail function for validation email address Add timeToString function for easy way to convert millis to formated time Add toast function for quick access to show Toast messages

--- app/src/main/java/com/optiontrade/br/Helper.kt
+++ app/src/main/java/com/optiontrade/br/Helper.kt
@@ -1,7 +1,18 @@
 package com.optiontrade.br
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.view.children
 import com.google.gson.Gson
+import java.io.IOException
+import java.io.InputStream
 
 private const val LOGTAG = "OptionTradeBR_LOG"
 
@@ -17,5 +28,45 @@ fun <T> fromJson(json: String, type: Class<T>): T {
     return Gson().fromJson(json, type)
 }
 
+fun ImageView.setImageAssets(context: Context, imgPath: String) {
+    try {
+        val ims: InputStream = context.assets.open(imgPath)
+        this.setImageDrawable(Drawable.createFromStream(ims, null))
+    } catch (ex: IOException) {
+        log("catch")
+        this.setImageDrawable(null)
+    }
+}
+
+fun changeVisibility(v1: View, v2: View) {
+    v2.visibility = v1.visibility
+    if (v1.visibility == View.VISIBLE) {
+        v1.visibility = View.GONE
+    } else {
+        v1.visibility = View.VISIBLE
+    }
+}
+
+
+fun CharSequence?.isValidEmail() =
+    !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+
+fun toast(context: Context, text: String) {
+    Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+}
+
+fun timeToString(millis: Long): String {
+    var seconds = "${((millis / 1000).toInt() % 60)}"
+    var minutes = "${(millis / (1000 * 60) % 60)}"
+    var hours = "${(millis / (1000 * 60 * 60))}"
+    if (((millis / 1000).toInt() % 60) < 10)
+        seconds = "0$seconds"
+    if ((millis / (1000 * 60) % 60) < 10)
+        minutes = "0$minutes"
+    if ((millis / (1000 * 60 * 60)) < 10)
+        hours = "0$hours"
+    return "$minutes:$seconds"
+}
 
 
